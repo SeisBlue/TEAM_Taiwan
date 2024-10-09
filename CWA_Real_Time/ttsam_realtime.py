37245b423a7e61e7f9e46991f72111fd228b894f
jimmy <jimmy60504@gmail.com>
2024-10-09 21:40:32 +0800

    black reformat, early return

--- CWA_Real_Time/ttsam_realtime.py
+++ CWA_Real_Time/ttsam_realtime.py
@@ -10,7 +10,6 @@ import argparse
 import json
 
 from obspy import UTCDateTime
-from pygmt.src import timestamp
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -24,14 +23,14 @@ buffer_time = 30  # 設定緩衝區保留時間
 sample_rate = 100  # 設定取樣率
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@socketio.on('connect')
+@socketio.on("connect")
 def connect_earthworm():
-    socketio.emit('connect_init')
+    socketio.emit("connect_init")
 
 
 def earthworm_wave_listener():
@@ -41,34 +40,40 @@ def earthworm_wave_listener():
         time.sleep(0.00001)
 
         wave = earthworm.get_wave(0)
-        if wave:
-            """
-            這裡並沒有去處理每個 trace 如果時間不連續的問題
-            """
-            try:
-                wave = convert_to_tsmip_legacy_naming(wave)
-                wave_id = join_id_from_dict(wave, order='NSLC')
 
-                # add new trace to buffer
+        if not wave:
+            continue
 
-                if wave_id not in wave_buffer.keys():
-                    wave_buffer[wave_id] = np.zeros(sample_rate * buffer_time)
-                    time_buffer[wave_id] = np.append(
-                        np.linspace(wave['startt'] - 14, wave['startt'],
-                                    sample_rate * (buffer_time - 1)),
-                        np.linspace(wave['startt'], wave['endt'], sample_rate))
+        """
+        這裡並沒有去處理每個 trace 如果時間不連續的問題
+        """
+        try:
+            wave = convert_to_tsmip_legacy_naming(wave)
+            wave_id = join_id_from_dict(wave, order="NSLC")
 
-                wave_buffer[wave_id] = np.roll(wave_buffer[wave_id],
-                                               -wave['data'].size)
-                wave_buffer[wave_id][-wave['data'].size:] = wave['data']
+            # add new trace to buffer
 
-                time_buffer[wave_id] = np.roll(time_buffer[wave_id],
-                                               -wave['data'].size)
-                time_buffer[wave_id][-wave['data'].size:] = np.linspace(
-                    wave['startt'], wave['endt'], wave['data'].size)
+            if wave_id not in wave_buffer.keys():
+                wave_buffer[wave_id] = np.zeros(sample_rate * buffer_time)
+                time_buffer[wave_id] = np.append(
+                    np.linspace(
+                        wave["startt"] - 14,
+                        wave["startt"],
+                        sample_rate * (buffer_time - 1),
+                    ),
+                    np.linspace(wave["startt"], wave["endt"], sample_rate),
+                )
 
-            except Exception as e:
-                print(e)
+            wave_buffer[wave_id] = np.roll(wave_buffer[wave_id], -wave["data"].size)
+            wave_buffer[wave_id][-wave["data"].size :] = wave["data"]
+
+            time_buffer[wave_id] = np.roll(time_buffer[wave_id], -wave["data"].size)
+            time_buffer[wave_id][-wave["data"].size :] = np.linspace(
+                wave["startt"], wave["endt"], wave["data"].size
+            )
+
+        except Exception as e:
+            print(e)
 
 
 def trigger_emitter(debug=False):
@@ -79,34 +84,38 @@ def trigger_emitter(debug=False):
     while True:
         try:
             if debug:
-                print('emit trigger', picks)
+                print("emit trigger", picks)
 
             trigger_msg = {}
             # pick 只有 Z 軸
             for pick_id, pick in picks.items():
-                network = pick['network']
-                station = pick['station']
-                location = pick['location']
-                channel = pick['channel']
+                network = pick["network"]
+                station = pick["station"]
+                location = pick["location"]
+                channel = pick["channel"]
 
                 data = {}
                 # 找到 wave_buffer 內的三軸資料
-                for i, component in enumerate(['Z', 'N', 'E']):
-                    wave_id = f'{network}.{station}.{location}.{channel[0:2]}{component}'
+                for i, component in enumerate(["Z", "N", "E"]):
+                    wave_id = (
+                        f"{network}.{station}.{location}.{channel[0:2]}{component}"
+                    )
                     print(wave_id)
                     data[component.lower()] = wave_buffer[wave_id].tolist()
 
-                trace_dict = {'traceid': pick_id,
-                              'time': time_buffer[pick_id].tolist(),
-                              'data': data}
+                trace_dict = {
+                    "traceid": pick_id,
+                    "time": time_buffer[pick_id].tolist(),
+                    "data": data,
+                }
 
-                trigger_msg[pick_id] = {'pick': pick, 'trace': trace_dict}
+                trigger_msg[pick_id] = {"pick": pick, "trace": trace_dict}
 
             # 資料傳給訊號前處理
             trigger_queue.put(trigger_msg)
 
             # 將資料傳送給前端
-            socketio.emit('trigger_data', trigger_msg)
+            socketio.emit("trigger_data", trigger_msg)
 
         except Exception as e:
             print(e)
@@ -116,53 +125,58 @@ def trigger_emitter(debug=False):
 def earthworm_convert_to_ttsam(debug=False):
     while True:
         trigger_msg = trigger_queue.get()
+
         if debug:
-            print('get trigger:', trigger_msg.keys())
-        if trigger_msg:
-            waveform = []
-            sta = []
-            target = []
-            station_name = []
-            for i, (pick_id, data) in enumerate(trigger_msg.items()):
-                if i == 0:
-                    start_time = data['trace']['time']
+            print("get trigger:", trigger_msg.keys())
 
-                trace = []
-                for j, component in enumerate(['Z', 'N', 'E']):
-                    trace.append(data['trace']['data'][component.lower()])
+        if not trigger_msg:
+            continue
 
-                waveform.append(trace)
-                sta.append([float(data['pick']['lat']), float(data['pick']['lon']), 100, 760])
-                target.append(
-                    [float(data['pick']['lat']), float(data['pick']['lon']), 100, 760])
-                station_name.append(data['pick']['station'])
+        waveform = []
+        sta = []
+        target = []
+        station_name = []
+        for i, (pick_id, data) in enumerate(trigger_msg.items()):
+            if i == 0:
+                start_time = data["trace"]["time"]
 
-            print(np.array(waveform).shape)
+            trace = []
+            for j, component in enumerate(["Z", "N", "E"]):
+                trace.append(data["trace"]["data"][component.lower()])
 
-            output = {
-                "waveform": waveform,
-                "sta": sta,
-                "target": target,
-                "station_name": station_name
-            }
+            waveform.append(trace)
+            sta.append(
+                [float(data["pick"]["lat"]), float(data["pick"]["lon"]), 100, 760]
+            )
+            target.append(
+                [float(data["pick"]["lat"]), float(data["pick"]["lon"]), 100, 760]
+            )
+            station_name.append(data["pick"]["station"])
 
-            start_timestamp = UTCDateTime(start_time[0]).isoformat()
-            with open(f'tests/data/ttsam_convert_{start_timestamp}.json', 'w') as json_file:
-                json.dump(output, json_file)
+        output = {
+            "waveform": waveform,
+            "sta": sta,
+            "target": target,
+            "station_name": station_name,
+        }
+
+        start_timestamp = UTCDateTime(start_time[0]).isoformat()
+        with open(f"tests/data/ttsam_convert_{start_timestamp}.json", "w") as json_file:
+            json.dump(output, json_file)
 
         time.sleep(1)
 
 
-def join_id_from_dict(data, order='NSLC'):
-    code = {'N': 'network', 'S': 'station', 'L': 'location', 'C': 'channel'}
-    data_id = '.'.join(data[code[letter]] for letter in order)
+def join_id_from_dict(data, order="NSLC"):
+    code = {"N": "network", "S": "station", "L": "location", "C": "channel"}
+    data_id = ".".join(data[code[letter]] for letter in order)
     return data_id
 
 
 def convert_to_tsmip_legacy_naming(wave):
-    if wave['network'] == 'TW':
-        wave['network'] = 'SM'
-        wave['location'] = '01'
+    if wave["network"] == "TW":
+        wave["network"] = "SM"
+        wave["location"] = "01"
 
     return wave
 
@@ -175,24 +189,27 @@ def earthworm_pick_listener(debug=False):
     """
     while True:
         pick_msg = earthworm.get_msg(buf_ring=2, msg_type=0)
-        if pick_msg:
-            # print(pick_msg)
-            try:
-                pick_data = parse_pick_msg(pick_msg)
-                pick_id = join_id_from_dict(pick_data, order='NSLC')
-                if pick_data['update_sec'] == '2':
-                    picks[pick_id] = pick_data
-                    socketio.emit('pick_data', pick_data)
-                    if debug:
-                        print('add pick:', pick_id)
-                elif pick_data['update_sec'] == '9':
-                    picks.__delitem__(pick_id)
-                    if debug:
-                        print('delete pick:', pick_id)
 
-            except Exception as e:
-                print(e)
-                continue
+        if not pick_msg:
+            continue
+
+        # print(pick_msg)
+        try:
+            pick_data = parse_pick_msg(pick_msg)
+            pick_id = join_id_from_dict(pick_data, order="NSLC")
+            if pick_data["update_sec"] == "2":
+                picks[pick_id] = pick_data
+                socketio.emit("pick_data", pick_data)
+                if debug:
+                    print("add pick:", pick_id)
+            elif pick_data["update_sec"] == "9":
+                picks.__delitem__(pick_id)
+                if debug:
+                    print("delete pick:", pick_id)
+
+        except Exception as e:
+            print(e)
+            continue
 
         time.sleep(0.00001)
 
@@ -201,40 +218,40 @@ def parse_pick_msg(pick_msg):
     pick_msg_column = pick_msg.split()
     try:
         pick = {
-            'station': pick_msg_column[0],
-            'channel': pick_msg_column[1],
-            'network': pick_msg_column[2],
-            'location': pick_msg_column[3],
-            'lon': pick_msg_column[4],
-            'lat': pick_msg_column[5],
-            'pga': pick_msg_column[6],
-            'pgv': pick_msg_column[7],
-            'pd': pick_msg_column[8],
-            'tc': pick_msg_column[9],  # Average period
-            'pick_time': pick_msg_column[10],
-            'weight': pick_msg_column[11],  # 0:best 5:worst
-            'instrument': pick_msg_column[12],  # 1:Acc 2:Vel
-            'update_sec': pick_msg_column[13]  # sec after pick
+            "station": pick_msg_column[0],
+            "channel": pick_msg_column[1],
+            "network": pick_msg_column[2],
+            "location": pick_msg_column[3],
+            "lon": pick_msg_column[4],
+            "lat": pick_msg_column[5],
+            "pga": pick_msg_column[6],
+            "pgv": pick_msg_column[7],
+            "pd": pick_msg_column[8],
+            "tc": pick_msg_column[9],  # Average period
+            "pick_time": pick_msg_column[10],
+            "weight": pick_msg_column[11],  # 0:best 5:worst
+            "instrument": pick_msg_column[12],  # 1:Acc 2:Vel
+            "update_sec": pick_msg_column[13],  # sec after pick
         }
 
-        pick['traceid'] = join_id_from_dict(pick, order='NSLC')
+        pick["traceid"] = join_id_from_dict(pick, order="NSLC")
 
         return pick
 
     except IndexError:
-        print('pick_msg parsing error:', pick_msg)
+        print("pick_msg parsing error:", pick_msg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--web', action='store_true',
-                        help='run web server')
-    parser.add_argument('--host', type=str, help='web server ip')
-    parser.add_argument('--port', type=int, help='web server port')
+    parser.add_argument("--web", action="store_true", help="run web server")
+    parser.add_argument("--host", type=str, help="web server ip")
+    parser.add_argument("--port", type=int, help="web server port")
     args = parser.parse_args()
 
-    earthworm = PyEW.EWModule(def_ring=1000, mod_id=2, inst_id=255,
-                              hb_time=30, db=False)
+    earthworm = PyEW.EWModule(
+        def_ring=1000, mod_id=2, inst_id=255, hb_time=30, db=False
+    )
     earthworm.add_ring(1000)  # buf_ring 0: Wave ring(tank player)
     earthworm.add_ring(1002)  # buf_ring 1: Wave ring 2
     earthworm.add_ring(1005)  # buf_ring 2: Pick ring
